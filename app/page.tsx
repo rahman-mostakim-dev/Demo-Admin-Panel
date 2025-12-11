06d6d4959640d066233398f34c1d52c172f6582b
Raiyan Hasan <sadmansabin7@gmail.com>
2025-12-11 15:57:17 +0600

    new-div

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
     <div >
       negga
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto, rem? Tenetur, voluptate eligendi. Temporibus quos, repellat ut voluptates perspiciatis ex beatae expedita eum odio doloremque, iure vitae, corporis dolores illum.</p>
+      <div>
+        hello world
+      </div>
     </div>
   );
 }
